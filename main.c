c1c76fc6b7730d9ec82721137c0596fddd4e41b0
Collin bartley <boloman94@gmail.com>
2024-09-29 23:54:13 -0400

    Code clean up

--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
-#include "rp2040_shift_register.h"
-#include "test.h"
-#include "onboard_led.h"
 #include <time.h>
 
 void blink_led(int num_times){
     int blink = 0;
     while(blink != num_times){
-        //on_board_led_toggle();
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(300);
-        //on_board_led_toggle();
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(300);
         blink++;
@@ -38,7 +33,17 @@ void blink_led(int num_times){
 #define FIFTEEN  0b0111111111111111
 #define SIXTEEN  0b1111111111111111
 
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t val){
+    //write the bits to the data pin
+    for(int i = 0; i < 8; i++) {
+        gpio_put(clockPin, 0);
 
+        gpio_put(dataPin, (val & 0x80) ? 1 : 0 );
+        val <<= 1;
+
+        gpio_put(clockPin, 1);
+    }
+}
 
 void writeTo595(uint16_t _data ) {
   // Output low level to latchPin
@@ -75,30 +80,8 @@ void main() {
     gpio_set_dir(LATCH_PIN, GPIO_OUT);
     on_board_led_init();
 
-    // Create new shift register
-    //struct shift_register_74hc595_t *myreg = new_shreg_74hc595(CLK_PIN, DATA_PIN, LATCH_PIN);
-
-//   while (true)
-//   {
-//     for (size_t qi = QA; qi <= QBP; qi++) {
-//         blink_led(qi);
-//         shreg_74hc595_put(myreg, qi, 1);
-//         sleep_ms(5000);
-//     }
-
-//     for (size_t qi = QA; qi <= QBP; qi++) {
-//         blink_led(qi);
-//         shreg_74hc595_put(myreg, qi, 0);
-//         sleep_ms(500);
-//     }
-//   }
     while(true){
-        // for (int j = 0; j < 8; j++) { // reset the LEDs
-        //     blink_led(j +1);
-        //     writeTo595(0);
-        //     //sleep_ms(400);
-        // }
-        // sleep_ms(100);
+        
         unsigned int x = 0b0000000000000000;   
         writeTo595(x);
         blink_led(1);
@@ -152,19 +135,11 @@ void main() {
         if(r == 15) {
             writeTo595(SIXTEEN);
         }
-
-        //for (int j = 0; j < 8; j++) { // Let led light up from right to left
-            //x <<= 1; // make the variable move one bit to left once, then the bright LED move one step to the left once.
-            sleep_ms(500);
-        //}
-        sleep_ms(100);
+        sleep_ms(600);
+        
         x = 0b00000000000000000;       //0b 0000 0000
-        //for (int j = 0; j < 8; j++) { // Let led light up from left to right
-            blink_led(2);
-            writeTo595(x);
-            //x >>= 1;    
-            //sleep_ms(500);
-        //}
+        blink_led(2);
+        writeTo595(x);
         sleep_ms(1500);
     }
 }
